e9d8866560084f8f575c7ba617370cf9ee5f0b30
Jason Evans <jason@galaxa.co>
2018-04-21 15:16:48 -0700

    initial commit

---
+++ src/components/SignIn.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Sign in thing</h1>
+            <button class="button" @click="authenticate">sign in</button>
+        
+        </div>
+    </div>
+</template>
+
+<script>
+import io from 'socket.io-client'
+const socket = io('http://localhost:4242')
+
+export default {
+  name: 'SignIn',
+  mounted () {
+      socket.open()
+  },
+  methods: {
+      authenticate() {
+          socket.emit('/auth')
+      }
+  }
+}
+</script>
