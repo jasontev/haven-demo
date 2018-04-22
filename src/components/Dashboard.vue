715f88d48a7993179d4fa9dc56d34d20acb1c6a2
Jason Evans <jason@galaxa.co>
2018-04-21 21:26:00 -0700

    new component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import io from 'socket.io-client'
+const socket = io('http://localhost:4242')
+
+export default {
+  name: 'SignIn',
+  mounted () {
+      socket.open()
+
+    socket.on('authenticated', (data) => {
+        console.log(data)
+        alert('authenticated ')
+    })
+  },
+  computed: {
+      username() {
+          return ''
+      },
+      email() {
+          return ''
+      }
+  }
+  methods: {
+      foo () {
+          
+      }
+  }
+}
+</script>
